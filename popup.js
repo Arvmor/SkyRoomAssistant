87aa38c690cba9b8aae57008738b5bb10bb25429
Arvin Morawej <Arvin.morawej@gmail.com>
2020-10-27 23:45:44 +0330

    Add auto login

--- popup.js
+++ popup.js
@@ -1,5 +1,8 @@
 let checkBox = document.getElementById('checkBox');
+let checkBox2 = document.getElementById('checkBox2');
+let btnAutoLogin = document.getElementById('btnAutoLogin');
 
+// first check box function for appearing volume bars
 checkBox.onclick = function(element) {
   if (checkBox.checked == true){
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -15,20 +18,33 @@ checkBox.onclick = function(element) {
                     });
           }
 };
-let checkBox2 = document.getElementById('checkBox2');
-
+// second check box function for hiding username characters
 checkBox2.onclick = function(element) {
   if (checkBox2.checked == true){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.executeScript(
-                tabs[0].id,
-                {code: 'document.getElementById("username").setAttribute("type", "password");'});
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.executeScript(
+              tabs[0].id,
+              {code: 'document.getElementById("username").setAttribute("type", "password");'});
             });
         }   else {
-                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                    chrome.tabs.executeScript(
-                        tabs[0].id,
-                        {code: 'document.getElementById("username").setAttribute("type", "text");'});
-                    });
-          }
-};
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    {code: 'document.getElementById("username").setAttribute("type", "text");'});
+                });
+            }
+        };
+
+// first button function for auto login
+btnAutoLogin.onclick = function(element) {
+var now = new Date();
+var msTillTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), document.getElementById("hour").value, document.getElementById("minute").value, document.getElementById("second").value, 0) - now;
+if (msTillTime < 0) {
+     msTillTime += 86400000; // it's after that time, try tomorrow.
+}
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: `alert("Automated Login set at ${document.getElementById("hour").value}:${document.getElementById("minute").value}:${document.getElementById("second").value}\\n${msTillTime}ms remaining\\n*Do NOT close this tab*");setTimeout(function(){document.getElementById("btn_login").click()}, ${msTillTime});`});
+      });
+    };
