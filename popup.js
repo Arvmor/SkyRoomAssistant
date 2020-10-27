d96c9af86fc9fb499a9cf73f6b01f60cae5208a7
Arvin Morawej <Arvin.morawej@gmail.com>
2020-10-27 14:05:03 +0330

    add username hider

--- popup.js
+++ popup.js
@@ -15,3 +15,20 @@ checkBox.onclick = function(element) {
                     });
           }
 };
+let checkBox2 = document.getElementById('checkBox2');
+
+checkBox2.onclick = function(element) {
+  if (checkBox2.checked == true){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.executeScript(
+                tabs[0].id,
+                {code: 'document.getElementById("username").setAttribute("type", "password");'});
+            });
+        }   else {
+                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                    chrome.tabs.executeScript(
+                        tabs[0].id,
+                        {code: 'document.getElementById("username").setAttribute("type", "text");'});
+                    });
+          }
+};
